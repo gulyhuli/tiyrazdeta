e389d72407c48a31f13ef47c39d266dd4cd46a77
maximKuzmin13 <maks.kuzmin13@gmail.com>
2018-05-14 16:01:36 +0300

    ‘12354657’

--- test3.py
+++ test3.py
@@ -14,32 +14,29 @@ def start(message):
 
 def hello(message):
     bot.send_message(message.chat.id, 'Приветствую, {name}. Выбери категорию.'.format(name=message.text))
-#    bot.register_next_step_handler(message,key)
-
-
-#@bot.message_handler(commands=['key'])
-#def key(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=False)
     markup.row('a')
     markup.row('b')
     markup.row('c')
     markup.row('d')
     markup.row('e')
-    bot.register_next_step_handler(message)
+    bot.register_next_step_handler(message, category)
 
 
 def category(message):
-    if message.text == 'a' :
-        bot.send_message(message.chat.id, 'Ты выбрал категорию А'.format(name=message.text))
-    elif message.text == 'b' :
-        bot.send_message(message.chat.id, 'Ты выбрал категорию Б'.format(name=message.text))
-        if message.text == 'c':
+        if message.text == 'a':
+            bot.send_message(message.chat.id, 'Ты выбрал категорию А'.format(name=message.text))
+        elif  message.text == 'b':
+            bot.send_message(message.chat.id, 'Ты выбрал категорию Б'.format(name=message.text))
+        elif message.text == 'c':
             bot.send_message(message.chat.id, 'Ты выбрал категорию C'.format(name=message.text))
-                elif message.text == 'e':
-        bot.send_message(message.chat.id, 'Ты выбрал категорию Е'.format(name=message.text))
-            if message.text == 'd':
-             bot.send_message(message.chat.id, 'Ты выбрал категорию Д'.format(name=message.text))
+        elif message.text == 'e':
+            bot.send_message(message.chat.id, 'Ты выбрал категорию Е'.format(name=message.text))
+        elif message.text == 'd':
+            bot.send_message(message.chat.id, 'Ты выбрал категорию Д'.format(name=message.text))
+        else:
+            bot.send_message(message.chat.id, 'Что то еще?'.format(name=message.text))
 
 
-bot.polling()
+bot.polling(none_stop=True, interval=0)
 
